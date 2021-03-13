150095d6b4584b9f98b513d65dd997308b8dbca5
SahilRajpal-hub <sahilrajpal.31586@gmail.com>
2021-03-13 18:55:02 +0530

    Router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import Header from './Componenets/Header'
 import Sidebar from './Componenets/Sidebar'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='App'>
+    <Router>
       <Header />
       <Sidebar />
-    </div>
+    </Router>
   )
 }
 
