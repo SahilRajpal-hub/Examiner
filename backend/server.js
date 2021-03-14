49f39b4521e83954c2b24629c6fd331afda264df
Sandeep Chaudhary <sandee.chaudhary367@gmail.com>
2021-03-14 12:59:59 +0530

    update 2

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express'
+import './db/mongoose'
 import dotenv from 'dotenv'
-import bodyParser from 'body-parser'
+// import bodyParser from 'body-parser'
 
 dotenv.config()
 const app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('API is running....')
@@ -15,6 +16,6 @@ const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(
-    `server running in ${process.env.NODE_ENV} mode and on ${process.env.PORT}`
+    `server running in ${process.env.NODE_ENV} mode and on ${PORT}`
   )
 })
